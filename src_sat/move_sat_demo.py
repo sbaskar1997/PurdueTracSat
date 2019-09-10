e258935906716cc77ea39340ccded5561fcf9657
Sandeep Baskar <Sandeep@pal-nat186-62-136.itap.purdue.edu>
2019-09-10 18:13:21 -0400

    added solenoid 2 to open as well

--- src_sat/move_sat_demo.py
+++ src_sat/move_sat_demo.py
@@ -9,18 +9,22 @@ def init_pi():
     pi = pigpio.pi()
     return pi
 
-def move_satellite_sol1(flight_time):
+def move_satellite(flight_time):
     # Check if time to move is 0
     if (time != 0):
         # Initialize pi and solenoid 1
         pi = init_pi()
-        sol1 = sol1(pi)
 
-        # Open solenoid 1
+        sol1 = sol1(pi)
+        sol2 = sol2(pi)
+
+        # Open solenoid 1 and 2
         sol1.open()
+        sol2.open()
 
         # Wait some time
         time.sleep(flight_time)
 
-        # Close solenoid 1
+        # Close solenoid 1 and 2
         sol1.close()
+        sol2.close()
