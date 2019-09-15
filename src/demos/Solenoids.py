5d91e47e4f73f38784181ac2eae024c4ce61fcf8
Sandeep Baskar <Sandeep@pal-nat186-49-250.itap.purdue.edu>
2019-09-15 17:22:17 -0400

    updated sol1

--- src/demos/Solenoids.py
+++ src/demos/Solenoids.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python
 # Python set of classes that open and close individual solenoids
+# import native modules
+import time
+
 class sol1:
     def __init__(self, pi):
         self._pi = pi
 
     def open(self):
+        self._pi.write(18,1)
+        time.sleep(.02)
         self._pi.write(18,0)
 
     def close(self):
+        self._pi.write(23,1)
+        time.sleep(.02)
         self._pi.write(23,0)
 
 class sol2:
