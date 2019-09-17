d8903785191372ad023aede7bc80a3cf268e2623
Alec <aleven@purdue.edu>
2019-09-17 18:58:23 -0400

    Update libraries/code for Raspberry Pi

--- Electronics/laser_plotting.py
+++ Electronics/laser_plotting.py
@@ -4,8 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import os
-#from gpiozero import LightSensor, Buzzer
-
+from gpiozero import LightSensor, Buzzer
 
 
 
@@ -17,23 +16,23 @@ index = count()
 # Initialize
 x_axis_start = 0
 x_axis_end = 10
-
 def animate(i):
-    data = pd.read_csv('data.csv')
-    #print(ldr.value)
-    x = data['x_value']
-    y1 = data['total_1']
-    print(len(x))
-    plt.cla()
-    plt.axis([len(x)-50, len(x)+50, 0, 1])
-    plt.plot(x, y1)
+	data = pd.read_csv('data.csv')
+	#print(ldr.value)
+	x = data['x_value']
+	y1 = data['total_1']
+	# print(len(x))
+	plt.cla()
+	plt.axis([len(x)-50, len(x)+50, 0, 1])
+	plt.plot(x, y1)
+
 
         #plt.axis([x_axis_start, x_axis_end, 0, 1])
     
 
 ani = FuncAnimation(plt.gcf(), animate, interval=100)
 
-plt.tight_layout()
+#plt.tight_layout()
 plt.show()
 
 
