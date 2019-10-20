1d6c06e6c82339d9cec0e469e2ae4e31e722b904
Dante <dsanaei@purdue.edu>
2019-10-20 17:07:39 -0400

    Add longer message capability

--- src/demos/LaserCom/Arduino Message Transfer/laser/laser.ino
+++ src/demos/LaserCom/Arduino Message Transfer/laser/laser.ino
@@ -1,24 +1,53 @@
 #define LASERPIN 12
+
+  // 01011010
+  //int bits[] = {LOW, HIGH, LOW, HIGH, HIGH, LOW, HIGH, LOW};
+  String myText = "Tracsat";
+  int bits[500];
+  int j;
+  int size;
+
+  
 void setup() {
   // put your setup code here, to run once:
   pinMode(LASERPIN, OUTPUT);
-  // 01011010
-  int bits[] = {LOW, HIGH, LOW, HIGH, HIGH, LOW, HIGH, LOW};
 
-}
+  
+  j = 0;
+  for(int i=0; i<myText.length(); i++){
+    char myChar = myText.charAt(i);
+    String binaryString = String((int) myChar, BIN);
+    for (int k = 0; k < 8; k++){
+      bits[i + k] = String(binaryString.charAt(k)).toInt();
+    }
+  }
+   size = myText.length() * 8;
+ for (int i; i > size; i++){
+  Serial.print(bits[i]);
+  if (!(i%8)){
+     Serial.println("");
+  }
+ }
+ }
 
 void loop() {
   
+
   // Start bit
   digitalWrite(LASERPIN, HIGH);
   delay(10);
   digitalWrite(LASERPIN,LOW);
-
-  for(int i = 0; i < 8; i++){
+  
+  for(int i = j; i < 8 + j; i++){
     digitalWrite(LASERPIN, bits[i]);
     delay(10);
   }
-
+  j+=8;
+  if (j >= size-8){
+    j = 0;
+  }
+  
+  
   digitalWrite(LASERPIN, LOW);
 
   delay(10);
