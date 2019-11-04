66e3120a34b34675291a568f8ec888e427749ce9
Sandeep Baskar <sbaskar@purdue.edu>
2019-11-04 17:35:27 -0500

    parallelized code

--- src/comp_vision/Camera.py
+++ src/comp_vision/Camera.py
@@ -3,6 +3,8 @@ import numpy as np
 import cv2
 import imutils
 import argparse
+import threading
+import time
 
 class Camera:
     # Contructor
@@ -14,21 +16,38 @@ class Camera:
         # Capture video from camera
         cap = cv2.VideoCapture(0)
 
-        while(1):
-            # Capture frame by frame
-            ret, frame = cap.read()
+        # Create threads for circle detect and reading/writing images
+        start = time.time()
+        circle_detect_worker = ThreadWorker(self.circle_detect)
+        read_and_write_worker = ThreadWorker(self.read_and_write)
+        iteration = 0
+        post_frame = None
 
-            #gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-            cv2.imwrite('frame.jpg', frame)
-            image = cv2.imread('frame.jpg')
-            post_frame = self.circle_detect(image)
+        while(1):
+            # Read and write each image from camera
+            read_and_write_worker.start((cap,))
+            image = read_and_write_worker.get_results()
+
+            # Detect circle from image
+            circle_detect_worker.start((image,))
+            #post_frame = self.circle_detect(image)
+            if (iteration > 50):
+                post_frame = circle_detect_worker.get_results()
+                iteration = 0
+                circle_det_ran = 1
+            else:
+                circle_det_ran = 0
 
 
 
             # cv2.putText(image, '%.2fft' % (inches/12),
             # (image.shape[1] - 200, image.shape[0] - 20), cv2.FONT_HERSHEY_SIMPLEX,
             # 2.0, (0, 255, 0), 3)
-            cv2.imshow('image', post_frame)
+            if (post_frame is not None) and (circle_det_ran):
+                cv2.imshow('image', post_frame)
+            else:
+                cv2.imshow('image', image)
+            iteration = iteration + 1
 
             # Quit if prompted to quit (press q)
             if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -45,7 +64,11 @@ class Camera:
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
         # detect circles in the image
-        circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1.2, 400)
+        circles_thread = ThreadWorker(cv2.HoughCircles)
+        circles_thread.start((gray,cv2.HOUGH_GRADIENT, 1.2, 150,))
+        circles = circles_thread.get_results()
+        #circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1.2, 400)
+
 
         # ensure at least some circles were found
         if circles is not None:
@@ -57,7 +80,8 @@ class Camera:
                 # draw the circle in the output image, then draw a rectangle
                 # corresponding to the center of the circle
                 # only draw circle if it is small enough to be considered an actual object
-                if (r < 70):
+                if (r < 100):
+                    print(r)
                     eta = 11.875/54;                # actual_distance/r_assoc (use one test case to calibrate correction factor)
                     offset = r * eta - 11.875;      # offset for moving object closer or greater
                     dist_act = 11.875 - offset      # subtract offset to reference distance
@@ -69,6 +93,66 @@ class Camera:
         else:
             return image
 
+    @staticmethod
+    def read_and_write(cap):
+        ret, frame = cap.read()
+
+        #gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        cv2.imwrite('frame.jpg', frame)
+        image = cv2.imread('frame.jpg')
+        return image
+
+
+class ThreadWorker():
+    '''
+    The basic idea is given a function create an object.
+    The object can then run the function in a thread.
+    It provides a wrapper to start it,check its status,and get data out the function.
+    '''
+    def __init__(self,func):
+        self.thread = None
+        self.data = None
+        self.func = self.save_data(func)
+
+    def save_data(self,func):
+        '''modify function to save its returned data'''
+        def new_func(*args, **kwargs):
+            self.data=func(*args, **kwargs)
+
+        return new_func
+
+    def start(self,params = None):
+        self.data = None
+        if self.thread is not None:
+            if self.thread.isAlive():
+                return 'running' #could raise exception here
+
+        #unless thread exists and is alive start or restart it
+        if params is not None:
+            self.thread = threading.Thread(target=self.func,args=params)
+        else:
+            self.thread = threading.Thread(target=self.func)
+        self.thread.start()
+        return 'started'
+
+    def status(self):
+        if self.thread is None:
+            return 'not_started'
+        else:
+            if self.thread.isAlive():
+                return 'running'
+            else:
+                return 'finished'
+
+    def get_results(self):
+        if self.thread is None:
+            return 'not_started' #could return exception
+        else:
+            if self.thread.isAlive():
+                self.thread.join()
+                return self.data
+            else:
+                return self.data
 
 
 camera = Camera('sat1')
