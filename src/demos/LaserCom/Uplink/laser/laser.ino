302ef13b45da661dcd14029d2b56eb25b09ce3f6
Dante <dsanaei@purdue.edu>
2019-11-12 16:31:26 -0500

    Clean up laser and receiver code

--- src/demos/LaserCom/Uplink/laser/laser.ino
+++ src/demos/LaserCom/Uplink/laser/laser.ino
@@ -4,83 +4,62 @@
   char receivedChars[numChars];
   boolean newData = false;
   boolean inputRetreived = false;
-  //String myText = "1,100,90,500#";
-  String myText = receivedChars;
   int bits[500];
-  int j;
+  int byteIndex;
   int size;
   void(* resetFunc) (void) = 0;
   
 void setup() {
-  // put your setup code here, to run once:
+  // Setup code
   pinMode(LASERPIN, OUTPUT);
   Serial.begin(9600);
+
+  //Wait for serial input.
   Serial.println("Waiting for data...");  
   while (inputRetreived == false){
     recvWithStartEndMarkers();
     showNewData();
   }
-  String myText = receivedChars;
-  
-  j = 0;
-  int position =  0;
-  //Serial.println(myText.length());
-  for(int i=0; i<myText.length(); i++){
-    char myChar = myText.charAt(i);
-    
-    String binaryString = String((int) myChar, BIN);
-    //Serial.println(binaryString);
-    if (binaryString.length() < 8){
-      for (int u = 0; u < 8 - binaryString.length(); u++){
-        bits[position] = 0;
-        position = position + 1;
-
-      }
-    }
-    //Serial.println(bits[position]);
-    for (int k = 0; k < binaryString.length(); k++){
-      bits[position] = String(binaryString.charAt(k)).toInt();
-      //Serial.println(bits[position]);
-      position = position + 1;
-      
-    }
-  }
-   size = myText.length() * 8;
-  int m = 0;
- for (int i=0; i < size; i++){
-  Serial.print(bits[i]);
-  if (!((i+1)%8) && i != 0){
-    Serial.println(myText.charAt(m++));
-  }
- }
- Serial.println("Starting...");
+  String serialInput = receivedChars;
+ 
+  convertToBinary(serialInput);
+  Serial.println("Starting...");
 }
 
 
 void loop() {
-  // Start bit
+  
+  //If '0' is inputted in the serial monitor, then the program will reset for a different set of data to be inputted. 
   if (Serial.available() > 0 && Serial.read() == 48) {
     resetFunc();
   }
+
+  //Start flash
   digitalWrite(LASERPIN, HIGH);
   delay(1);
   digitalWrite(LASERPIN,LOW);
-  for(int i = j; i < 8 + j; i++){
+
+  //Send one byte
+  for(int i = byteIndex; i < 8 + byteIndex; i++){
     digitalWrite(LASERPIN, bits[i]);
     //Serial.print(bits[i]);
     delayMicroseconds(1000);
   }
-  j+=8;
+  byteIndex+=8;
   //Serial.print(" ");
-  if (j >= size){
+
+  //Reset the byteIndex back to 0 when the serialInput has been fully sent
+  if (byteIndex >= size){
     //Serial.println("");
-    j = 0;
+    byteIndex = 0;
   }
+
+  //Delay between bytes.
   digitalWrite(LASERPIN, LOW);
   delay(5);
 }
 
-
+//Function to receive serial input
 void recvWithStartEndMarkers() {
     static boolean recvInProgress = false;
     static byte ndx = 0;
@@ -114,6 +93,7 @@ void recvWithStartEndMarkers() {
     }
 }
 
+//Show serial input
 void showNewData() {
     if (newData == true) {
         Serial.print("Inputted Message: ");
@@ -121,4 +101,39 @@ void showNewData() {
         inputRetreived = true;
         newData = false;
     }
+}
+
+//Convert serial input to binary
+void convertToBinary(String serialInput){
+  byteIndex = 0;
+  int position =  0;
+  size = serialInput.length() * 8;
+  int printIndex = 0;
+  
+  //Serial.println(serialInput.length());
+  for(int i=0; i<serialInput.length(); i++){
+    char myChar = serialInput.charAt(i);
+    String binaryString = String((int) myChar, BIN);
+    //Serial.println(binaryString);
+    if (binaryString.length() < 8){
+      for (int u = 0; u < 8 - binaryString.length(); u++){
+        bits[position] = 0;
+        position = position + 1;
+      }
+    }
+    //Serial.println(bits[position]);
+    for (int k = 0; k < binaryString.length(); k++){
+      bits[position] = String(binaryString.charAt(k)).toInt();
+      //Serial.println(bits[position]);
+      position = position + 1; 
+    }
+  }
+
+  //Print all bytes for visual inspection
+  for (int i=0; i < size; i++){
+    Serial.print(bits[i]);
+    if (!((i+1)%8) && i != 0){
+      Serial.println(serialInput.charAt(printIndex++));
+    }
+  }
 }
