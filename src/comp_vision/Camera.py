13eff7e7a5b29b522f657a70fcb214b021a01673
Sandeep Baskar <sbaskar@purdue.edu>
2019-11-15 18:52:52 -0500

    refactored circle detect code

--- src/comp_vision/Camera.py
+++ src/comp_vision/Camera.py
@@ -13,49 +13,20 @@ class Camera:
     # Contructor
     def __init__(self, id):
         self._id = id
+        self._cap = None
 
-    # Open camera
-    def read_distance(self):
-        # Capture video from camera
-        cap = cv2.VideoCapture(0)
+    # Start camera
+    def start_camera(self):
+        self._cap = cv2.VideoCapture(0)
 
-        # Create threads for circle detect and reading/writing images
-        circle_detect_worker = Threader(self.circle_detect)
-        read_and_write_worker = Threader(self.read_and_write)
-        iteration = 0
-        post_frame = None
+    # Read and write images
+    def read_and_write(self):
+        ret, frame = self._cap.read()
+        cv2.imwrite('frame.jpg', frame)
+        image = cv2.imread('frame.jpg')
+        return image
 
-        while(1):
-            # Read and write each image from camera
-            read_and_write_worker.start((cap,))
-            image = read_and_write_worker.get_results()
-
-            # Detect circle from image at every 25 frames
-            circle_detect_worker.start((image,))
-            if (iteration > 25) and not circle_det_ran:
-                post_frame = circle_detect_worker.get_results()
-                iteration = 0
-                circle_det_ran = 1
-            else:
-                circle_det_ran = 0
-
-            # If circle detection code ran, show image, if not do not show image
-            if (post_frame is not None) and (circle_det_ran):
-                cv2.imshow('image', post_frame)
-            else:
-                cv2.imshow('image', image)
-
-            # Update frame count
-            iteration = iteration + 1
-
-            # Quit if prompted to quit (press q)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-
-        # Release and destroy everything when the simulation is over
-        cap.release()
-        cv2.destroyAllWindows()
-
+    # Circle detection and distance reading
     @staticmethod
     def circle_detect(image):
         # load the image, clone it for output, and then convert it to grayscale
@@ -64,9 +35,10 @@ class Camera:
 
         # detect circles in the image
         circles_thread = Threader(cv2.HoughCircles)
-        circles_thread.start((gray,cv2.HOUGH_GRADIENT, 1.2, 200,))
+        circles_thread.start((gray,cv2.HOUGH_GRADIENT, 1.0, 100,))
         circles = circles_thread.get_results()
         #circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1.2, 400)
+        circle_detected = False
 
 
         # ensure at least some circles were found
@@ -79,24 +51,55 @@ class Camera:
                 # draw the circle in the output image, then draw a rectangle
                 # corresponding to the center of the circle
                 # only draw circle if it is small enough to be considered an actual object
-                if (r < 200):
+                if (r < 100 and r > 0):
+                    print('eh')
                     eta = 11.875/54;                # actual_distance/r_assoc (use one test case to calibrate correction factor)
                     offset = r * eta - 11.875;      # offset for moving object closer or greater
                     dist_act = 11.875 - offset      # subtract offset to reference distance
+
+                    # Draw circle
                     cv2.circle(output, (x, y), r, (0, 255, 0), 4)
                     cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+                    circle_detected = True
 
             # show the output image
-            return output
+            return [output, circle_detected]
         else:
-            return image
+            return [image, False]
 
     @staticmethod
-    def read_and_write(cap):
-        ret, frame = cap.read()
-        cv2.imwrite('frame.jpg', frame)
-        image = cv2.imread('frame.jpg')
-        return image
+    def show_image(image):
+        cv2.imshow('img', image)
 
 camera = Camera('sat1')
-camera.read_distance()
+
+# Start camera
+camera.start_camera()
+
+# Initialize loop parameters
+circle_detected = False         # Initialize variable to break loop
+iteration = 0                   # Iterator to determine whether or not to run circle detect code
+
+# Initialize worker threads to parallelize code
+circle_detect_worker = Threader(camera.circle_detect)
+read_and_write_worker = Threader(camera.read_and_write)
+
+while(1):
+    # Read and write images from camera
+    read_and_write_worker.start()
+    image = read_and_write_worker.get_results()
+
+    # Check whether or not to run circle detection code
+    if (iteration > 10):
+        circle_detect_worker.start((image,))
+        [output_image, circle_detected] = circle_detect_worker.get_results()
+        iteration = 0
+    else:
+        output_image = image
+
+    # If circle detected break loop
+    if (circle_detected):
+        break
+
+    # Increment iteration
+    iteration += 1
