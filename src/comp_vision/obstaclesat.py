86a3703447139f09e6907c26f2c82235485c5de9
aleven-1 <aleven@purdue.edu>
2019-12-08 19:04:32 -0500

    /

--- src/comp_vision/obstaclesat.py
+++ src/comp_vision/obstaclesat.py
@@ -3,7 +3,7 @@
 # Import native modules
 import numpy as np
 import time
-
+import sys
 # Import custom modules
 from Camera import Camera
 #from easygopigo3 import EasyGoPiGo3
@@ -21,7 +21,7 @@ def purge_pi(pi):
     for n in ports:
          pi.write(n,0)
 def move_forward(time_flight = 1):
-    tstart = time.time()
+    #tstart = time.time()
     if (time_flight != 0):
         pi = init_pi()
         purge_pi(pi)
@@ -32,20 +32,28 @@ def move_forward(time_flight = 1):
         sol1.close()
         sol2.close()
     #Open solenoid 1
+        trun = 1.5
         sol1.open()
-   return(tstart)
-def stop(tnow)
+        time.sleep(trun)
+        sol1.close()
+        #
+   #return(tstop)
+def stop()
    tbrake = time.time()
    sol1.close()
-   while ((time.time()-tbrake)<tnow):
+   while ((time.time()-tbrake)<1.5):
     #sol1.close()
-    sol2.open()
-    
+        sol2.open()
    sol2.close()
 
+def turnright()
+    time.sleep(2)
+    pi.set_servo_pulsewidth(17,1800)
+    time.sleep(2)
+    pi.set_servo_pulsewidth(17,1100)
 def obstaclesat():
- 
-    tstart = move_forward()  
+    pi.set_servo_pulsewidth(17,1100)  
+    move_forward()  
     [circle_detected, center, obj_distance] = detection_demo()
  
     #if (center[0] > 320):
@@ -56,9 +64,9 @@ def obstaclesat():
         #rotate_dir = 1
     
     while (circle_detected):
-        tnow = tstart - time.time() #find time to keep braking solenoid on
+        #tnow = tstart - time.time() #find time to keep braking solenoid on
         [circle_detected, center, obj_distance] = detection_demo(infinite_loop = False)
-        stop(tnow) #use stop function
+        stop() #use stop function
 
     #Close all solenoids after ten seconds
     time.sleep(10)
