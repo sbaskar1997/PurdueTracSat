2df16e86c3f69e87d38b7cbd95be05c3de32eb5c
Dante <dsanaei@purdue.edu>
2020-03-15 03:39:02 -0400

    Start Uplink GUI

---
+++ src/groundstation/fullscreen.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+
+class Fullscreen_Example:
+    def __init__(self):
+        self.window = tk.Tk()
+        self.fullScreenState = False
+        self.window.attributes("-fullscreen", self.fullScreenState)
+
+        self.w, self.h = self.window.winfo_screenwidth(), self.window.winfo_screenheight()
+        self.window.geometry("%dx%d" % (self.w, self.h))
+        
+        self.window.bind("<F11>", self.toggleFullScreen)
+        self.window.bind("<Escape>", self.quitFullScreen)
+
+        self.window.mainloop()
+
+    def toggleFullScreen(self, event):
+        self.fullScreenState = not self.fullScreenState
+        self.window.attributes("-fullscreen", self.fullScreenState)
+
+    def quitFullScreen(self, event):
+        self.fullScreenState = False
+        self.window.attributes("-fullscreen", self.fullScreenState)
+
+if __name__ == '__main__':
+    app = Fullscreen_Example()
